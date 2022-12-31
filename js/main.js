d36ffd85f29f08edde127fbe8ebd6f53c81ebd52
Julio Pampuch <juliopampuch@gmail.com>
2022-12-31 09:55:44 -0300

    project finished

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ const form = document.getElementById("novoItem")
 const list = document.getElementById('lista')
 const localStorageItems = JSON.parse(localStorage.getItem("items")) || []
 
-const items = []
-
 console.log(localStorageItems)
 
 localStorageItems.forEach((item) => {
@@ -15,24 +13,28 @@ form.addEventListener('submit', (event) => {
   const nome = event.target.elements['nome']
   const quantidade = event.target.elements['quantidade']
 
-  const exists = localStorageItems.find((element) => {element.nome === nome.value})
+  const exists = localStorageItems.find(element => element.nome === nome.value)
 
-  console.log(exists)
-  
   const currentItem = {
     "nome": nome.value,
     "quantidade": quantidade.value
   }
   
-  createElement(currentItem)
+  if(exists) {
+    currentItem.id = exists.id
+    updateElement(currentItem)
+    localStorageItems[localStorageItems.findIndex(element => element.id === exists.id)] = currentItem
+  } else {
+    currentItem.id = localStorageItems[localStorageItems.length - 1] ? (localStorageItems[localStorageItems.length - 1]).id + 1 : 0
+    console.log(localStorageItems[localStorageItems.length - 1])
+    createElement(currentItem)
+    localStorageItems.push(currentItem)
+  }
 
-  items.push(...localStorageItems, currentItem)
-  localStorage.setItem("items", JSON.stringify(items))
-
+  localStorage.setItem("items", JSON.stringify(localStorageItems))
   nome.value = ''
   quantidade.value = ''
 })
-
 
 function createElement(item) {
 
@@ -40,6 +42,7 @@ function createElement(item) {
   newItem.classList.add('item')
 
   const itemNumber = document.createElement('strong')
+  itemNumber.dataset.id = item.id
   itemNumber.innerHTML = item.quantidade
 
   newItem.appendChild(itemNumber)
@@ -47,4 +50,26 @@ function createElement(item) {
 
   list.appendChild(newItem)
 
+  newItem.appendChild(deleteBtn(item.id))
+}
+
+function updateElement(item) {
+  (document.querySelector("[data-id='"+item.id+"']")).innerHTML = item.quantidade 
+}
+
+function deleteBtn(id) {
+  const btnEelement = document.createElement('button')
+  btnEelement.innerText = 'x'
+
+  btnEelement.addEventListener('click', (event) => {
+    deleteElement(event.target.parentNode, id)
+  })
+  
+  return btnEelement
+}
+
+function deleteElement(tag, id) {
+  localStorageItems.splice(localStorageItems.findIndex(element => element.id === id), 1)
+  tag.remove()
+  localStorage.setItem('items', JSON.stringify(localStorageItems))
 }
